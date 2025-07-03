74fda4b029d89dc73b96ea7ddf7d6cfef48c9fc8
kaizhou24 <130264951+kaizhou24@users.noreply.github.com>
2025-07-03 01:16:08 -0400

    Add Player class and deal/draw methods

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-class Deck {
+class Deck{
     constructor(){
-        const suits = ["h", "d", "c", "s"]; //Hearts, Diamonds, Clubs, Spades
         const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        const suits = ["h", "d", "c", "s"]; //Hearts, Diamonds, Clubs, Spades
         this.cards = ["w", "w"]; //Initialize the deck with Jokers
 
-        for(const suit of suits){
-            for(const rank of ranks){
-                this.cards.push(suit + rank);
+        for(const rank of ranks){
+            for(const suit of suits){
+                this.cards.push(rank + suit);
             }
         }
 
@@ -14,7 +14,7 @@ class Deck {
     }
 
     shuffle(){
-        for(let i = 0; i < this.cards.length; i++){
+        for(let i = 0; i < this.cards.length; i++) {
             let random_i = Math.floor(Math.random() * i);
             let temp = this.cards[i];
             this.cards[i] = this.cards[random_i];
@@ -23,9 +23,33 @@ class Deck {
         console.log("deck shuffled");
     }
 
+    deal(){
+        if(this.cards.length !== 0){
+            return this.cards.pop();
+        }else{
+            console.log("no more cards");
+            return undefined;
+        }
+    }
+}
 
+class Player{
+    constructor(name){
+        this.name = name;
+        this.hand = [];
+    }
+
+    draw(deck){
+        const card = deck.deal();
+        if(card){
+            this.hand.push(card);
+            console.log(`${this.name} drew ${card}`);
+        }
+    }
 }
 
 let deck = new Deck();
 deck.shuffle();
-console.log(deck);
+const player1 = new Player("alice");
+player1.draw(deck);
+console.log(player1);
