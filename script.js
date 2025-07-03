5acbdd2b5341b19c13a6f2044395a6e674ec80ba
kaizhou24 <130264951+kaizhou24@users.noreply.github.com>
2025-07-03 00:54:48 -0400

    Refactor deck creation and shuffling

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-function createDeck(){
-    let suits = ["h", "d", "c", "s"];
-    let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-    const deck = ["w", "w"]; //Create the deck with Jokers
+class Deck {
+    constructor(){
+        const suits = ["h", "d", "c", "s"]; //Hearts, Diamonds, Clubs, Spades
+        const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        this.cards = ["w", "w"]; //Initialize the deck with Jokers
 
-    for(const suit of suits){
-        for(const rank of ranks){
-            deck.push(suit + rank);
+        for(const suit of suits){
+            for(const rank of ranks){
+                this.cards.push(suit + rank);
+            }
         }
+
+        console.log("deck created");
     }
 
-    return deck;
-}
-
-function shuffleDeck(deck){
-    for(let i = 0; i < deck.length; i++){
-        let random_i = Math.floor(Math.random() * i);
-        let temp = deck[i];
-        deck[i] = deck[random_i];
-        deck[random_i] = temp;
+    shuffle(){
+        for(let i = 0; i < this.cards.length; i++){
+            let random_i = Math.floor(Math.random() * i);
+            let temp = this.cards[i];
+            this.cards[i] = this.cards[random_i];
+            this.cards[random_i] = temp;
+        }
+        console.log("deck shuffled");
     }
 
-    return deck;
+
 }
 
-let deck = createDeck();
-shuffleDeck(deck);
+let deck = new Deck();
+deck.shuffle();
 console.log(deck);
